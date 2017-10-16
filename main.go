a299974ac2416eb56b89855500b61241fb678027
etiennedi <etienne@dilocker.de>
2017-10-16 21:41:35 +0200

    create splits, navigate to path, run command
    don't create the first split as its already there, but only following

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -27,7 +28,14 @@ func main() {
 	window, err := config.GetWindow(windowName)
 	FatalOn(err)
 
-	log.Print(window)
+	err = exec.Command("tmux", "new-window").Run()
+	FatalOn(err)
+
+	err = exec.Command("tmux", "rename-window", windowName).Run()
+	FatalOn(err)
+
+	err = window.CreateSplits()
+	FatalOn(err)
 }
 
 type Split struct {
@@ -35,9 +43,64 @@ type Split struct {
 	Path    string `yaml:"path"`
 }
 
+func (s Split) NavigateToPath() error {
+	if s.Path == "" {
+		return errors.New("path in split must be set")
+	}
+
+	shellCommand := fmt.Sprintf("cd %s", s.Path)
+	err := exec.Command("tmux", "send-keys", shellCommand, "Enter").Run()
+	if err != nil {
+		return fmt.Errorf("could not navigate to path '%s': %s", s.Path, err)
+	}
+
+	return nil
+}
+
+func (s Split) RunCommand() error {
+	if s.Command == "" {
+		return errors.New("command in split must be set")
+	}
+
+	err := exec.Command("tmux", "send-keys", s.Command, "Enter").Run()
+	if err != nil {
+		return fmt.Errorf("could not navigate to path '%s': %s", s.Path, err)
+	}
+
+	return nil
+}
+
 type Window struct {
 	Splits []Split `yaml:"splits"`
 }
+
+func (w *Window) CreateSplits() error {
+	if len(w.Splits) == 0 {
+		return errors.New("No splits configured for this window")
+	}
+
+	for i, split := range w.Splits {
+		if i != 0 {
+			err := exec.Command("tmux", "split-window", "-v").Run()
+			if err != nil {
+				return err
+			}
+		}
+
+		err := split.NavigateToPath()
+		if err != nil {
+			return err
+		}
+
+		err = split.RunCommand()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 type Config struct {
 	Windows map[string]*Window `yaml:"windows"`
 }
